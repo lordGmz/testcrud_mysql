7aa3db47ea1dfde93bc3db71ad074da1e9758888
lordGmz <59732700+lordGmz@users.noreply.github.com>
2020-10-26 21:23:22 +0000

    remove on username update form streamListener.cancel();

--- lib/screens/setting/usernames/usernamesUpdate.dart
+++ lib/screens/setting/usernames/usernamesUpdate.dart
@@ -57,7 +57,6 @@ class _UsernamesUpdateState extends State<UsernamesUpdate> {
   @override
   void dispose() {
     // TODO: implement dispose
-    streamListener.cancel();
     super.dispose();
   }
 
